c9437024d9c35ae06eee55f09edb1feb25b67725
sshankarsateesh <42050390+sshankarsateesh@users.noreply.github.com>
2020-02-24 14:54:14 -0600

    Update: README.md notebooks

--- notebooks/Users/sshankar.sateesh@gmail.com/notebook1.py
+++ notebooks/Users/sshankar.sateesh@gmail.com/notebook1.py
@@ -1,2 +1,11 @@
 # Databricks notebook source
-
+import urllib, json
+import requests
+import pandas as pd
+url = "https://api.stlouisfed.org/fred/sources?api_key=ee8afb7752f0cf5dea9bb634737b7c50&file_type=json"
+r = requests.get(url)
+print(r.json())
+data1 = pd.json_normalize(r.json())
+print(data1['sources'][0])
+data2 = pd.json_normalize(data1['sources'][0])
+print(data2)
